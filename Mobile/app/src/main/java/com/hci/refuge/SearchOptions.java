bee23f7d9eb160cce9dcbb5aa4ccb4e2872bf526
bellsworth13 <becca42195@gmail.com>
2016-04-27 18:59:25 -0400

    Added files via upload

---
+++ Mobile/app/src/main/java/com/hci/refuge/SearchOptions.java
@@ -0,0 +1,19 @@
+package com.hci.refuge;
+
+/**
+ * Created by becca13 on 4/27/16.
+ */
+public enum SearchOptions {
+    Food,
+    Housing,
+    Clothes,
+    Doctor,
+    Health,
+    Transportation,
+    Jobs,
+    Hospital,
+    Shelter,
+    Language,
+    Education,
+    Other
+}
